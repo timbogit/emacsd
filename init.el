9f3c6ec8bbd5253a8ac725099329c23e7ec168e8
Tim Schmelmer <tim.schmelmer@livingsocial.com>
2015-02-11 22:08:43 -0700

    changing the init.el for use with clojure / cider

--- init.el
+++ init.el
@@ -1,36 +1,7 @@
-;; Set up some directory vars
-(defvar home-dir)
-(setq home-dir (concat (expand-file-name "~") "/"))
-(defvar dot-emacs-dir (concat home-dir ".emacs.d/"))
-(defvar emacs-plugins-dir (concat dot-emacs-dir "site-lisp/"))
-;; Set up load path 
-(setq load-path (append (list (concat home-dir "")
-			      (concat emacs-plugins-dir "color-theme")
-			      ;;(concat emacs-plugins-dir "emacs-colors-solarized")
-                              (concat emacs-plugins-dir "emacs-color-theme-solarized")
-			      (concat emacs-plugins-dir "rhtml")
-                              (concat emacs-plugins-dir "emacs-nav-49")
-			      (concat dot-emacs-dir "site-lisp"))
-                        load-path))
-;; load the themes and ack plugin
-(require 'color-theme)
-(require 'color-theme-solarized)
-(color-theme-solarized-dark)
-;;(require 'ack)
-;;(setq ack-command "ack")
-;;; rhtml-mode
-(require 'rhtml-mode)
-(add-hook 'rhtml-mode-hook
-	  (lambda () (rinari-launch)))
-;; nav settings
-(require 'nav)
-(nav-disable-overeager-window-splitting)
-;; Optional: set up a quick key to toggle nav
-(global-set-key [f8] 'nav-toggle)
 
-;; Interactively Do Things
-(require 'ido)
-(ido-mode t)
+;; ;; nav settings
+;; (require 'nav)
+;; (nav-disable-overeager-window-splitting)
 
 ;;; This was installed by package-install.el.
 ;;; This provides support for the package system and
@@ -41,11 +12,6 @@
 ;    (load
 ;     (expand-file-name "~/.emacs.d/elpa/package.el"))
 ;  (package-initialize))
-;(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-(add-hook 'yaml-mode-hook
-      '(lambda ()
-           (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
 
 ;; tab to spaces settings, and highlight trailing white-space
 ;;(setq c-basic-indent 2)
@@ -58,17 +24,68 @@
 ;;; add ClojureMode major mode
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
-
+ 
 (when (not package-archive-contents)
   (package-refresh-contents))
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-eshell starter-kit-ruby clojure-mode)
+(defvar my-packages '(starter-kit 
+                      starter-kit-eshell 
+                      starter-kit-ruby
+                      better-defaults
+                      clojure-mode
+                      cider
+                      company
+                      paredit
+                      exec-path-from-shell
+                      projectile
+                      flx-ido
+                      magit
+                      ack
+                      yaml-mode
+                      solarized-theme
+                      neotree)
   "A list of packages to ensure are installed at launch.")
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+
+(require 'ido)
+(ido-mode t)
+(require 'flx-ido)
+(ido-everywhere t)
+(flx-ido-mode t)
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
+ 
+(global-company-mode)
+(projectile-global-mode)
+ 
+(add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+ 
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(setq nrepl-hide-special-buffers t)
+
+;; color theme
+(load-theme 'solarized-dark t)
+
+;; yaml-mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-hook 'yaml-mode-hook
+     '(lambda ()
+          (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+
+;; adding ack
+(require 'ack)
+
+;; neotree - like NERDtree for emacs
+;; Optional: set up a quick key to toggle nav
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
 
 ;; Switch off the automatic insertion of the # -*- coding: utf-8 -*-
 ;; on top of Ruby mode files
